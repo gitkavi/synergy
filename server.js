d9d90ec3c8614bee0ccf5d2dfac8114c3beb5c6c
kavitha <kavitha.veluchamy@gmail.com>
2018-05-29 11:45:40 -0400

    server code changed

--- server.js
+++ server.js
@@ -35,9 +35,9 @@ if (process.env.NODE_ENV === "production") {
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", function(req, res) {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
